7c3c82c22715f3c489e0775097b2996a91e864bf
Andrew <amccan5@uwo.ca>
2018-09-22 18:26:11 -0400

    Add users, static pages, testing and links.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # root home page
   root 'static_pages#home'
 
+  # users
+  get '/signup', to: 'users#new'
+
   # static page routing
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 end
