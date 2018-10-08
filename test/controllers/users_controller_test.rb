82e91f59ac785d10b34ca8db53d99db116ed8f69
Andrew <amccan5@uwo.ca>
2018-10-08 19:44:37 -0400

    User updates, admin status.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,11 +4,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @base_title = "Cupped"
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
   
   test "should get new" do
     get signup_path
     assert_response :success
     assert_select 'title', "Sign up | #{@base_title}"
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              '',
+                                            password_confirmation: '',
+                                            admin: 1 } }
+    assert_not @other_user.admin?
   end
 end
