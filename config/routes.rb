002762178fa45770eb36d4fa791c8d8618889bac
Andrew <amccan5@uwo.ca>
2018-10-08 21:52:04 -0400

    Account activation.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # static page routing
   get '/help', to: 'static_pages#help'
